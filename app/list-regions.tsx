39d6111adf97086d8f4c10314336535e223dd5d7
Thabet Alyahya <ThabetFy@outlook.com>
2024-02-19 11:48:29 +0300

    feat: Add Welcome page

---
+++ app/list-regions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+
+export default function ListRegions() {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>This is the ListRegions screen</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
